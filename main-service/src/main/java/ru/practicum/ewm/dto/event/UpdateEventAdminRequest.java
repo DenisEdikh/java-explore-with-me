2fb093846169aaf9ea0fe24b17f26da0febf78e7
Denis <denisedikh@yandex.ru>
2024-11-11 14:46:04 +0300

    убрал интерфейс, сделал абстрактный класс

--- main-service/src/main/java/ru/practicum/ewm/dto/event/UpdateEventAdminRequest.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/event/UpdateEventAdminRequest.java
@@ -1,37 +1,15 @@
 package ru.practicum.ewm.dto.event;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.practicum.ewm.model.EventStateActionAdmin;
-import ru.practicum.ewm.model.Location;
-import ru.practicum.ewm.validator.DateEventConstraint;
 
-import java.time.LocalDateTime;
-
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class UpdateEventAdminRequest implements UpdateEventRequest {
-    @Size(min = 20, max = 2000)
-    private String annotation;
-    @Positive
-    private Long category;
-    @Size(min = 20, max = 7000)
-    private String description;
-    @DateEventConstraint(value = 2)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime eventDate;
-    private Location location;
-    private Boolean paid;
-    @PositiveOrZero
-    private Integer participantLimit;
-    private Boolean requestModeration;
+public class UpdateEventAdminRequest extends UpdateEventRequest {
     private EventStateActionAdmin stateAction;
-    @Size(min = 3, max = 120)
-    private String title;
 }
--- main-service/src/main/java/ru/practicum/ewm/dto/event/UpdateEventRequest.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/event/UpdateEventRequest.java
@@ -1,27 +1,37 @@
 package ru.practicum.ewm.dto.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.practicum.ewm.model.Location;
+import ru.practicum.ewm.validator.DateEventConstraint;
 
 import java.time.LocalDateTime;
 
-public interface UpdateEventRequest {
-    String getAnnotation();
-
-    Long getCategory();
-
-    String getDescription();
-
-    LocalDateTime getEventDate();
-
-    Location getLocation();
-
-    Boolean getPaid();
-
-    Integer getParticipantLimit();
-
-    Boolean getRequestModeration();
-
-    String getTitle();
-
-    Object getStateAction();
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateEventRequest {
+    @Size(min = 20, max = 2000)
+    private String annotation;
+    @Positive
+    private Long category;
+    @Size(min = 20, max = 7000)
+    private String description;
+    @DateEventConstraint(value = 2)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime eventDate;
+    private Location location;
+    private Boolean paid;
+    @PositiveOrZero
+    private Integer participantLimit;
+    private Boolean requestModeration;
+    @Size(min = 3, max = 120)
+    private String title;
 }
